e3f7ddb3676465a1a118552b2a9cac6943d447a1
mila-fox <luda_grischuk@tut.by>
2022-12-19 23:35:36 +0300

    fix: updated path to scripts

--- pages/main/main.js
+++ pages/main/main.js
@@ -23,6 +23,57 @@ function handleInput() {
     testimonialsList.style.transform = `translateX(-${value*(cardWidth)}px)`;
 }
 
+const cards = [
+    {
+        photo: '../../assets/images/Rectangle 5.jpg',
+        title: {
+            first: 'giant Pandas',
+            second: 'Native to Southwest China',
+        },
+        food: '../../assets/icons/banana-bamboo_icon.svg',
+    },
+    {
+        photo: '../../assets/images/Rectangle 6.jpg',
+        title: {
+            first: 'Eagles',
+            second: 'Native to South America',
+        },
+        food: '../../assets/icons/meet-fish_icon.svg',
+    },
+    {
+        photo: '../../assets/images/Rectangle 7.jpg',
+        title: {
+            first: 'Gorillas',
+            second: 'Native to Congo',
+        },
+        food: '../../assets/icons/banana-bamboo_icon.svg',
+    },
+    {
+        photo: '../../assets/images/Rectangle 8.png',
+        title: {
+            first: 'Two-toed Sloth',
+            second: 'Mesoamerica, South America',
+        },
+        food: '../../assets/icons/banana-bamboo_icon.svg',
+    },
+    {
+        photo: '../../assets/images/Rectangle 9.png',
+        title: {
+            first: 'cheetahs',
+            second: 'Native to Africa',
+        },
+        food: '../../assets/icons/meet-fish_icon.svg',
+    },
+    {
+        photo: '../../assets/images/Penguins.png',
+        title: {
+            first: 'Penguins',
+            second: 'Native to Antarctica',
+        },
+        food: '../../assets/icons/meet-fish_icon.svg',
+    },
+];
+
 const testimonialsCards = document.querySelectorAll('.testimonials-card');
 for (let card of testimonialsCards) {
     card.addEventListener('click', showFullCard);
